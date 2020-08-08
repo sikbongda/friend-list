e17c6554f1b1a29647092c04efbe00086b9a41ae
sikbongda <jys1016@gmail.com>
2020-08-08 23:25:19 +0900

    Add data class for friend profile.

---
+++ app/src/main/java/com/studyfork/sfoide/model/FriendProfile.kt
@@ -0,0 +1,12 @@
+package com.studyfork.sfoide.model
+
+data class FriendProfile(
+    val name: String,
+    val age: Int,
+    val gender: String,
+    val country: String,
+    val email: String,
+    val phoneNumber: String,
+    val cellPhoneNumber: String,
+    val photoUrl: String
+)

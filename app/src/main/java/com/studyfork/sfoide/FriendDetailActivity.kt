a2768d531de2e6979abbd3dca0cbcb23995ebead
sikbongda <jys1016@gmail.com>
2020-08-10 01:21:04 +0900

    FriendDetailActivity 생성

---
+++ app/src/main/java/com/studyfork/sfoide/FriendDetailActivity.kt
@@ -0,0 +1,11 @@
+package com.studyfork.sfoide
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class FriendDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_friend_detail)
+    }
+}
